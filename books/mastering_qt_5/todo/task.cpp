2eaecc4aca60742ea7d7ca2a643ec186d2ab6b3b
Quan Hua <quanhua92@gmail.com>
2017-09-11 13:26:55 +0700

    + Add chapter 1 of Mastering QT 5

---
+++ books/mastering_qt_5/todo/task.cpp
@@ -0,0 +1,58 @@
+#include <QInputDialog>
+#include "task.h"
+#include "ui_task.h"
+
+Task::Task(const QString& name, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Task)
+{
+    ui->setupUi(this);
+
+    setName(name);
+
+    connect(ui->editButton, &QPushButton::clicked, this, &Task::rename);
+    connect(ui->removeButton, &QPushButton::clicked, [this] {
+        emit removed(this);
+    });
+    connect(ui->checkbox, &QCheckBox::toggled, this, &Task::checked);
+}
+
+void Task::setName(const QString& name)
+{
+    ui->checkbox->setText(name);
+}
+
+QString Task::name() const
+{
+    return ui->checkbox->text();
+}
+
+void Task::rename()
+{
+    bool ok;
+    QString value = QInputDialog::getText(this, tr("Edit task"),
+                                          tr("Task name"),
+                                          QLineEdit::Normal,
+                                          this->name(), &ok);
+    if (ok && !value.isEmpty()){
+        setName(value);
+    }
+}
+
+void Task::checked(bool checked)
+{
+    QFont font(ui->checkbox->font());
+    font.setStrikeOut(checked);
+    ui->checkbox->setFont(font);
+    emit statusChanged(this);
+}
+
+bool Task::isCompleted() const
+{
+    return ui->checkbox->isChecked();
+}
+
+Task::~Task()
+{
+    delete ui;
+}
